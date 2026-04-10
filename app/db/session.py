5e8b71d145a028f1dd6cbd2d7ed0ad1580fca030
Ahmed25890 <swaxigaming@gmail.com>
2026-04-10 14:34:24 +0200

    config settins

--- app/db/session.py
+++ app/db/session.py
@@ -1,10 +1,8 @@
 from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession, async_sessionmaker
 from sqlalchemy.orm import declarative_base
-import os
-from dotenv import load_dotenv
-load_dotenv()
+from app.core.config import settings
 
-DATABASE_URL = os.getenv("DATABASE_URL")
+DATABASE_URL = settings.DATABASE_URL
 
 engine = create_async_engine(DATABASE_URL, echo=False, future=True)
 
